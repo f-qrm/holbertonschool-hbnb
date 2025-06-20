853bb436f94bf4af75f52c4a2673ac9306e9e375
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-20 10:01:35 +0200

    feat(api): register reviews namespace in Flask app
    Added the /api/v1/reviews namespace to the API via Flask-RESTx.
    This integrates the reviews endpoints into the main application routing.

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,6 +2,7 @@ from app.api.v1.users import api as users_ns
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.reviews import api as reviews_ns
 
 
 def create_app():
@@ -9,8 +10,7 @@ def create_app():
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added 
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
