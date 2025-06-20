faec3dd714b116749415bf5a16bd1b40bc343333
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-20 13:47:58 +0200

    fix: add missing 's' to namespace's place

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(places_ns, path='/api/v1/place')
+    api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
