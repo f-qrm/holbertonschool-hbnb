c0f952ec53be9dd54aeff16803c708c35a9237c7
f-qrm <qerimifjolla7@gmail.com>
2025-07-01 09:03:23 +0200

    feat(auth): implement JWT-based login and protected routes using flask-jwt-extended
    - Added `flask-jwt-extended` to requirements and configured JWTManager in app factory
    - Created `/api/v1/auth/login` endpoint for user authentication and JWT token generation
    - JWT token includes user ID and is_admin claim for role-based access
    - Secured protected routes using @jwt_required()
    - Added example protected endpoint to demonstrate token validation and identity extraction
    
    This allows stateless authentication and enables secure access to protected resources via JWT.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
 from config import DevelopmentConfig
+from flask_jwt_extended import JWTManager
+from app.api.v1.auth import api as auth_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
 
+jwt = JWTManager()
 def create_app(config_class="config.DevelopmentConfig"):
     """
     Application factory to create and configure the Flask app instance.
@@ -25,11 +29,14 @@ def create_app(config_class="config.DevelopmentConfig"):
     - Initializes the Flask-RESTx API and registers namespaces.
     """
     app = Flask(__name__)
+    bcrypt = Bcrypt(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     app.config.from_object(config_class)
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(auth_ns, path="/api/v1/auth")
+    jwt.init_app(app)
 
     return app
