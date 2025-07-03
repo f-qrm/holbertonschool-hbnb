8b748c71b24b349fcc06a7aaaba5ef300faf7327
f-qrm <qerimifjolla7@gmail.com>
2025-07-03 09:53:24 +0200

    refactor: Updated to import BaseModel from the new location

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,7 +1,6 @@
 from app import db
 from abc import ABC, abstractmethod
-from app import db  # Assuming you have set up SQLAlchemy in your Flask app
-from app.models import User, Place, Review, Amenity  # Import your models
+from app.models import User, Place, Review, Amenity
 
 class Repository(ABC):
     @abstractmethod
