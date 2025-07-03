a847370cca5f51a0e6b089ef9f0cdc2604917a5e
f-qrm <qerimifjolla7@gmail.com>
2025-07-03 09:53:43 +0200

    refactor: Updated to import BaseModel from the new location

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -29,45 +29,8 @@ Usage example:
     text="Great place!")
 """
 
-import uuid
-from datetime import datetime
+from baseclass import BaseModel
 
-
-class BaseModel:
-    """Base model providing unique ID and timestamp management."""
-
-    def __init__(self, id=None, created_at=None, updated_at=None):
-        """
-        Initialize a new BaseModel instance.
-
-        Args:
-            id (str, optional): Unique identifier. Generated if None.
-            created_at (datetime, optional): Creation timestamp. Defaults
-            to now.
-            updated_at (datetime, optional): Last update timestamp. Defaults
-            to now.
-        """
-        self.id = id if id else str(uuid.uuid4())
-        self.created_at = created_at if created_at else datetime.now()
-        self.updated_at = updated_at if updated_at else datetime.now()
-
-    def save(self):
-        """
-        Update the `updated_at` timestamp to the current time.
-        """
-        self.updated_at = datetime.now()
-
-    def update(self, data):
-        """
-        Update attributes of the instance based on a dictionary.
-
-        Args:
-            data (dict): Dictionary of attributes to update.
-        """
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save()
 
 
 class Review(BaseModel):
