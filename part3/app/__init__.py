72b95b46b3b4b668f2e6c52acbc6787e297208b5
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-03 16:11:30 +0200

    fix(app): remove duplicate JWTManager init and fix config import

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from config import DevelopmentConfig
 from flask_jwt_extended import JWTManager
 from config import DevelopmentConfig
+import config
 
 from app.api.v1.admin import api as admin_ns
 from app.api.v1.auth import api as auth_ns
@@ -19,7 +19,7 @@ db = SQLAlchemy()
 
 jwt = JWTManager()
 
-def create_app(config_class="config.DevelopmentConfig"):
+def create_app(config_class=config.DevelopmentConfig):
     """
     Application factory to create and configure the Flask app instance.
 
@@ -50,6 +50,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(auth_ns, path="/api/v1/auth")
     api.add_namespace(admin_ns, path='/api/v1/admin')
     db.init_app(app)
-    jwt.init_app(app)
 
     return app
